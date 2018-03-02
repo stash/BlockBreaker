1731e8dae81895dd5fcf58e28c4807faa322a43f
Jeremy Stashewsky <jstash@gmail.com>
2018-03-02 15:43:00 -0800

    Simple menu system

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+    public void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
