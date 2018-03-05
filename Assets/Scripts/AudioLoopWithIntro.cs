d4cf3da805e6c31768aa8c6cca66534462d6c431
Jeremy Stashewsky <jstash@gmail.com>
2018-03-04 16:08:15 -0800

    Add looping audio player

---
+++ Assets/Scripts/AudioLoopWithIntro.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class AudioLoopWithIntro : MonoBehaviour {
+    public AudioSource introSource;
+    public AudioSource loopSource;
+
+    static AudioLoopWithIntro instance;
+
+    void Awake() {
+        if (instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    void Start() {
+        DontDestroyOnLoad(gameObject);
+
+        double introEnd = AudioSettings.dspTime + introSource.clip.length;
+        // start the intro clip immediately
+        introSource.PlayScheduled(AudioSettings.dspTime);
+        introSource.SetScheduledEndTime(introEnd);
+
+        // schedule the loop clip to begin as soon as the intro clip ends.
+        loopSource.PlayScheduled(introEnd);
+    }
+}
