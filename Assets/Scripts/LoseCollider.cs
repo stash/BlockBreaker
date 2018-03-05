da793fd6013a71ba2f21c2c0dbd78e4afd19187f
Jeremy Stashewsky <jstash@gmail.com>
2018-03-04 17:59:11 -0800

    Paddle control with mouse

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LoseCollider : MonoBehaviour {
     public SceneController sceneController;
