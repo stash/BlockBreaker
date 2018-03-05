8b708d8f21c50f0a53482ae840580eafadb3969f
Jeremy Stashewsky <jstash@gmail.com>
2018-03-04 17:11:36 -0800

    Simple ball and game-ending colliders

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+    public SceneController sceneController;
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("boop");
+        sceneController.LoadScene("GameOver");
+    }
+}
