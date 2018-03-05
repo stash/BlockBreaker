512ec232247a8125c6de23d9f1377f5eb62d9ea8
Jeremy Stashewsky <jstash@gmail.com>
2018-03-04 17:59:39 -0800

    Refactor: cleanup PaddleController

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -3,16 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PaddleController : MonoBehaviour {
-
     const float fieldUnits = 16f;
     const float paddleWidth = 1f;
 
-    void Start () {
-		
-	}
-	
 	void Update () {
-        Debug.Log(fieldUnits * Input.mousePosition.x / Screen.width);
         float mosuePos = fieldUnits * Input.mousePosition.x / Screen.width;
         mosuePos = Mathf.Clamp(mosuePos, paddleWidth / 2, fieldUnits - paddleWidth / 2);
         transform.position = new Vector3(mosuePos, transform.position.y, transform.position.z);
