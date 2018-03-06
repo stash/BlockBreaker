c7af32dadb2705ae2fcbbc88af3bfd3ca51574b1
Jeremy Stashewsky <jstash@gmail.com>
2018-03-05 17:26:29 -0800

    Brick prefabs and controller

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+    public int maxHits = 0;
+
+    private int timesHit;
+
+    void Start() {
+        timesHit = 0;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        BallController ball = collision.gameObject.GetComponent<BallController>();
+        if (ball == null) return;
+        Debug.Log(ball);
+        timesHit++;
+    }
+
+    void Update() {
+        if (timesHit >= maxHits) {
+            DestroyObject(gameObject);
+        }
+    }
+}
