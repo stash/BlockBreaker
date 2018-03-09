77d9596174e2afedd506a56cd7d674d93e48618f
Jeremy Stashewsky <jstash@gmail.com>
2018-03-09 14:55:57 -0800

    Tweak velocity when the ball collides w/ something.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,21 +2,28 @@
 
 public class BallController : MonoBehaviour {
     public Vector2 launchVelocity = new Vector2(1f, 10f);
+    public float tweakMagnitude = 0.2f;
 
     private PaddleController paddle;
     private bool launched;
+    private bool ending;
     private Vector3 paddleToBall;
+    private Rigidbody2D rb;
 
     void Start() {
         paddle = FindObjectOfType<PaddleController>(); // find active paddle
 
         paddleToBall = transform.position - paddle.transform.position;
-        GetComponent<Rigidbody2D>().isKinematic = true;
 
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+
+        ending = false;
         SceneController.OnSceneEnding += SceneController_OnSceneEnding;
     }
 
     void OnDestroy() {
+        ending = true;
         SceneController.OnSceneEnding -= SceneController_OnSceneEnding;
     }
 
@@ -27,7 +34,6 @@ public class BallController : MonoBehaviour {
         transform.position = paddle.transform.position + paddleToBall;
 
         if (Input.GetMouseButtonDown(0)) {
-            Rigidbody2D rb = GetComponent<Rigidbody2D>();
             // launch and relinquish control to physics subsystem
             rb.isKinematic = false;
             rb.velocity = launchVelocity;
@@ -36,9 +42,18 @@ public class BallController : MonoBehaviour {
     }
 
     void SceneController_OnSceneEnding() {
+        ending = true;
         // Stop the ball if the scene is ending
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.isKinematic = true;
         rb.velocity = Vector2.zero;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (ending) return;
+        Vector2 tweak = new Vector2(
+            Random.Range(-tweakMagnitude, tweakMagnitude), 
+            Random.Range(-tweakMagnitude, tweakMagnitude)
+        );
+        rb.velocity += tweak;
     }
 }
