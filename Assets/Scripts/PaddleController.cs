a2621a4d415dab6e70e7d24c33016ae82c46b561
Jeremy Stashewsky <jstash@gmail.com>
2018-03-09 15:12:21 -0800

    Implement autoplaying.

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
 
 public class PaddleController : MonoBehaviour {
+    public bool autoPlay = false;
+
+    BallController ball;
+
     const float fieldUnits = 16f;
     const float paddleWidth = 1f;
     const float paddleHalfWidth = paddleWidth / 2f;
     const float minMouseX = paddleHalfWidth;
     const float maxMouseX = fieldUnits - paddleHalfWidth;
 
+    void Start() {
+        ball = FindObjectOfType<BallController>();
+    }
+
     void Update() {
-        float mosuePos = fieldUnits * Input.mousePosition.x / Screen.width;
-        mosuePos = Mathf.Clamp(mosuePos, minMouseX, maxMouseX);
-        transform.position = new Vector3(mosuePos, transform.position.y, transform.position.z);
+        float x;
+        if (autoPlay) {
+            x = ball.transform.position.x;
+        } else {
+            x = fieldUnits * Input.mousePosition.x / Screen.width;
+        }
+        MoveToX(x);
+    }
+
+    void MoveToX(float x) {
+        x = Mathf.Clamp(x, minMouseX, maxMouseX);
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
     }
 }
