3328e72223d3cf2b80166b095b6b970ed7eb1941
Jeremy Stashewsky <jstash@gmail.com>
2018-03-09 15:46:46 -0800

    Third attempt at ball velocity tweaking

---
+++ Assets/Scripts/BallTweaker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTweaker : MonoBehaviour {
+    public Vector2 min;
+    public Vector2 max;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        BallController ball = collision.gameObject.GetComponent<BallController>();
+        if (!ball) return;
+        Vector2 tweak = new Vector2(
+            Random.Range(min.x, max.x),
+            Random.Range(min.y, max.y)
+        );
+        ball.TweakVelocity(tweak);
+    }
+}
